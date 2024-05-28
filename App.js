311cd4c5221f23141dfe4d9289fbe3abc70796b0
Mike Lim <dongseok.lim@gmail.com>
2024-05-27 19:25:22 -0500

    [feat] js-mobile-game: add assets and update initial components

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
+
+export const ImageAssets = {
+  backGroundImage: require("./src/assets/background.png"),
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground
+        source={ImageAssets.backGroundImage}
+        resizeMode="cover"
+        style={styles.background}
+      />
+      <StatusBar hidden />
     </View>
   );
 }
@@ -14,8 +22,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  background: {
+    flex: 1,
+    justifyContent: "center",
   },
 });
