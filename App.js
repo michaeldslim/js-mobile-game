2dea8f2c3b4b99700848952ccf021ab2abf74657
Mike Lim <dongseok.lim@gmail.com>
2024-05-28 21:51:07 -0500

    [feat] js-mobile-game: add new libraries and add gameplay button

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faPlay } from "@fortawesome/free-solid-svg-icons/faPlay";
 
 export const ImageAssets = {
   backGroundImage: require("./src/assets/background.png"),
@@ -14,6 +16,9 @@ export default function App() {
         resizeMode="cover"
         style={styles.background}
       />
+      <View style={styles.gameButton}>
+        <FontAwesomeIcon icon={faPlay} size={42} />
+      </View>
       <StatusBar hidden />
     </View>
   );
@@ -22,9 +27,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "column",
+    position: "relative",
   },
   background: {
     flex: 1,
+  },
+  gameButton: {
+    position: "absolute",
     justifyContent: "center",
+    alignItems: "center",
+    width: 60,
+    height: 60,
+    marginTop: 20,
+    backgroundColor: "wheat",
+    borderWidth: 4,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
   },
 });
